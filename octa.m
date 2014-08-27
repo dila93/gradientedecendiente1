dc6191b5b4a9864c826cd48856245f30c1748669
dila93 <sebas.dila@gmail.com>
2014-08-26 20:17:05 -0500

    Update octa.m

--- octa.m
+++ octa.m
@@ -1,13 +1,7 @@
+%26/agosto/2014
 x=load('ex2x.dat');
 y=load('ex2y.dat');
-
-
-
-m=length(x);
-
-tmp0=0;
-tmp1=0;
-
+m=length(x); %longitud del muestreo
 i=0;
 sum0=0;
 tetha0=0;
@@ -19,18 +13,18 @@ while (i <= 1500)
   j=1;
   sum0=0;
   while(j<=m)
-    sum0=sum0+((tetha0+tetha1*x(j))-y(j));
+    sum0=sum0+((tetha0+tetha1*x(j))-y(j)); %sumatoria del primer tetha
     j=j+1;
   endwhile
-tempTetha0=tetha0-(alpha/m)*sum0;
+tempTetha0=tetha0-(alpha/m)*sum0; %primera ecuacion
 
   j=1;
   sum1=0;
   while(j<=m)
-    sum1=sum1+(((tetha0+tetha1*x(j))-y(j))*x(j));
+    sum1=sum1+(((tetha0+tetha1*x(j))-y(j))*x(j)); %sumatoria del segundo tetha
     j=j+1;
   endwhile
-tempTetha1=tetha1-(alpha/m)*sum1;
+tempTetha1=tetha1-(alpha/m)*sum1; %segunda ecuacion
   
   tetha0 = tempTetha0;
   tetha1 = tempTetha1;
@@ -47,7 +41,7 @@ disp(m);
 hold on;
 plot(x,tetha0+tetha1*x);
 
-figure
+figure %continuara xD
 
 
 
